6434d6f64a412c699b378d9857c1b2c29a23122b
Kelvin zawala <87924723+Zawala@users.noreply.github.com>
2023-08-24 16:32:19 +0200

    keras branch migrate

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from __future__ import division, print_function
-from collections import OrderedDict
-from torch.autograd import Variable
-from PIL import Image
-import numpy as np
-import os
-import torch
-import torch.nn as nn
-from torchvision import models, datasets, transforms
-from torchvision.models import ResNet18_Weights
+# -*- coding: utf-8 -*-
+"""
+Created by Divyansh Mathur
+"""
 
+from __future__ import division, print_function
 # coding=utf-8
 import sys
 import os
 import glob
 import re
+import cv2
 import numpy as np
+import tensorflow as tf
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+# Keras
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
@@ -23,105 +32,75 @@ from werkzeug.utils import secure_filename
 
 # Define a flask app
 app = Flask(__name__)
-def load_model(filepath):
-        
-        checkpoint = torch.load(filepath)
-        model = models.resnet18()
-        classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(512, 512)),
-                          ('relu', nn.ReLU()),
-                          #('dropout1', nn.Dropout(p=0.2)),
-                          ('fc2', nn.Linear(512, 39)),
-                          ('output', nn.LogSoftmax(dim=1))
-                          ]))
 
-        # Replacing the pretrained model classifier with our classifier
-        model.fc = classifier
-        model.load_state_dict(checkpoint['state_dict'])
-        return model, checkpoint['class_to_idx']
-model, class_to_idx = load_model('res18tomato_checkpoint.pth')
+# Model saved with Keras model.save()
+MODEL_PATH ='model_inception.h5'
 
-idx_to_class = { v : k for k,v in class_to_idx.items()}
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# Load your trained model
+model = load_model(MODEL_PATH)
 
 
 
-def process_image(image):
-        width, height = image.size
+
+def model_predict(img_path, model):
+    print(img_path)
+    img = image.load_img(img_path)
+
+    # Crop the image to a square shape if necessary
+    width, height = img.size
+    if width != height:
         min_dim = min(width, height)
         left = (width - min_dim) // 2
         top = (height - min_dim) // 2
         right = (width + min_dim) // 2
         bottom = (height + min_dim) // 2
-        image = image.crop((left, top, right, bottom))
-        print(image.size)
-        size = 256, 256
-        image.thumbnail(size, Image.Resampling.LANCZOS)
-        image = image.crop((128 - 112, 128 - 112, 128 + 112, 128 + 112))
-        npImage = np.array(image)
-        npImage = npImage/255.
-        
-        imgA = npImage[:,:,0]
-        imgB = npImage[:,:,1]
-        imgC = npImage[:,:,2]
+        img = img.crop((left, top, right, bottom))
     
-        imgA = (imgA - 0.485)/(0.229) 
-        imgB = (imgB - 0.456)/(0.224)
-        imgC = (imgC - 0.406)/(0.225)
-        
-        npImage[:,:,0] = imgA
-        npImage[:,:,1] = imgB
-        npImage[:,:,2] = imgC
-    
-        npImage = np.transpose(npImage, (2,0,1))
-    
-        return npImage
+    # Resize the image to the target size
+    img = img.resize((224, 224))
+    x = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2YCrCb)
+    x = cv2.resize(x, (224, 224))
+    x = np.expand_dims(x, axis=0)
+    x[..., 0] = x[..., 0] - 128.0
+    x[..., 1:] = x[..., 1:] - 128.0
+    x = x.astype('float32')
+   
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x=x/255
+   
 
-def score_frame(pil_image):
-        """
-        function scores each frame of the video and returns results.
-        :param frame: frame to be infered.
-        :return: labels and coordinates of objects found.
-
-        """
-        topk=1
-        image_np = np.array([process_image(Image.open(pil_image))])
-
-        image = torch.FloatTensor(image_np)
-        model.eval()
-        output = model.forward(Variable(image))
-        pobabilities = torch.exp(output).data.numpy()[0]
-
-        top_idx = np.argsort(pobabilities)[-topk:][::-1] 
-        top_class = [idx_to_class[x] for x in top_idx]
-        top_probability = pobabilities[top_idx]
-
-        return top_probability, top_class
-    
-
-def model_predict(img_path, model):
-        
-        # Preprocess the frame for AI inference
-
-         # Perform AI inference on the frame
-            prediction = score_frame(img_path)
-
-        # Draw boxes on regions of interest with some text
-            print(prediction[0][0])
-            print(prediction)
-            #try:
-            #    if prediction[0][0]>0.7:
-            label=prediction[1][0]
-                    
-            #    else:
-            #        label='background'
-            #except Exception as e:
-            #    pass
-           
-            return label
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+   # x = preprocess_input(x)
+    print(type(x))
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    print(preds)
+    if preds==0:
+        preds="Bacterial_spot"
+    elif preds==1:
+        preds="Early_blight"
+    elif preds==2:
+        preds="Late_blight"
+    elif preds==3:
+        preds="Leaf_Mold"
+    elif preds==4:
+        preds="Septoria_leaf_spot"
+    elif preds==5:
+        preds="Spider_mites Two-spotted_spider_mite"
+    elif preds==6:
+        preds="Target_Spot"
+    elif preds==7:
+        preds="Tomato_Yellow_Leaf_Curl_Virus"
+    elif preds==8:
+        preds="Tomato_mosaic_virus"
+    else:
+        preds="Healthy"
         
     
     
+    return preds
 
 
 @app.route('/', methods=['GET'])
